6fa32e616bd2ba7803241d3c830c67f5412e8860
Motiullah Sajit <sajitmotiullah@gamil.com>
2021-05-12 02:25:04 +0600

    row added

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import requests from '../../Requests';
 import Banner from '../Banner/Banner';
 import Nav from '../Nav/Nav';
 import Row from '../Row/Row';
@@ -9,10 +10,14 @@ const HomeScreen = () => {
         <main className='homeScreen'>
             <Nav />
             <Banner />
-            <Row/>
-            <Row/>
-            <Row/>
-            <Row/>
+            <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+            <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
+            <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
+            <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
+            <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies}/>
+            <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies}/>
+            <Row title='Romacne Movies' fetchUrl={requests.fetchRomanceMovies}/>
+            <Row title='Documentaris' fetchUrl={requests.fetchDocumentaries}/>
         </main>
     );
 };
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import axios from '../../axios';
+import React, { useEffect, useState } from 'react';
+import './Row.css';
 
-const Row = () => {
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+    const [moveis, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl])
+
     return (
-        <section>
-            
+        <section className='row'>
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {
+                    moveis.map(movie => (
+                        ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+                            <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                        )
+                    ))
+                }
+            </div>
         </section>
     );
 };
